7239b4a705a2da721923747fb81cbafa72e44c2a
endigo <camradis1996@gmail.com>
2017-08-16 11:06:01 +0300

    Change chat view

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Chatroom</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Chatroom
+                        <span class="badge pull-right">@{{ usersInRoom.length }}</span>
+                    </div>
 
-                <div class="panel-body" id="app">
                     <chat-log :messages="messages"></chat-log>
                     <chat-composer v-on:messagesent="addMessage"></chat-composer>
                 </div>
